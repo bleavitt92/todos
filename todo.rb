f41ca82860a3658f4be64a9a872045c4d15ce5c0
Betsy Leavitt <betsyleavitt@Betsys-MacBook.local>
2019-09-23 14:10:18 -0400

    Adding files to repository.

---
+++ todo.rb
@@ -0,0 +1,209 @@
+require "sinatra"
+require "sinatra/reloader"
+require "sinatra/content_for"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+# Assign an id to each todo list as added.
+def next_element_id(elements)
+  max = elements.map { |todo| todo[:id] }.max || 0
+  max + 1
+end
+
+def load_list(id)
+  list = session[:lists].find{ |list| list[:id] == id }
+  return list if list
+  redirect "/lists"
+end
+
+# Return an error message if the name is invalid. Return nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "List name must be between 1 and 100 characters."
+  elsif session[:lists].any? { |list| list[:name] == name }
+    "List name must be unique."
+  end
+end
+
+# Return an error message if the name is invalid. Return nil if name is valid.
+def error_for_todo(name)
+  if !(1..100).cover? name.size
+    "Todo must be between 1 and 100 characters."
+  end
+end
+
+helpers do
+  def list_complete?(list)
+    todos_count(list) > 0 && todos_remaining_count(list) == 0
+  end
+
+  def list_class(list)
+    "complete" if list_complete?(list)
+  end
+
+  def todos_count(list)
+    list[:todos].size
+  end
+
+  def todos_remaining_count(list)
+    list[:todos].select { |todo| !todo[:completed] }.size
+  end
+
+  def sort_lists(lists, &block)
+    complete_lists, incomplete_lists = lists.partition { |list| list_complete?(list) }
+
+    incomplete_lists.each { |list| yield list, lists.index(list) }
+    complete_lists.each { |list| yield list, lists.index(list) }
+  end
+
+  def sort_todos(todos, &block)
+    complete_todos, incomplete_todos = todos.partition { |todo| todo[:completed] }
+
+    incomplete_todos.each { |todo| yield todo, todos.index(todo) }
+    complete_todos.each { |todo| yield todo, todos.index(todo) }
+  end
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+# View list of lists
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get "/lists/new" do 
+  erb :new_list, layout: :layout
+end
+
+# View that todo list
+get "/lists/:id" do
+  @list_id = params[:id].to_i
+  @list = load_list(@list_id)
+  erb :single_list, layout: :layout
+end
+
+# Edit an existing todo list
+get "/lists/:id/edit" do
+  @list_id = params[:id].to_i
+  @list = load_list(@list_id)
+  erb :edit_list, layout: :layout
+end
+
+# Create a new list
+post "/lists" do
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    id = next_element_id(session[:lists])
+    session[:lists] << {name: list_name, todos: [], id: id}
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
+end
+
+# add a new todo to list 
+post "/lists/:list_id/todos" do
+  @list_id = params[:list_id].to_i
+  @list = load_list(@list_id)
+  text = params[:todo].strip
+
+  error = error_for_todo(text)
+  if error
+    session[:error] = error
+    erb :single_list, layout: :layout
+  else
+    id = next_element_id(@list[:todos])
+    @list[:todos] << { id: id, name: text, completed: false }
+    session[:success] = "The todo was added."
+    redirect "/lists/#{@list_id}"
+  end
+end
+
+# Update existing todo list
+post "/lists/:id" do
+  list_name = params[:list_name].strip
+  id = params[:id].to_i
+  @list = load_list(id)
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = "The list has been updated."
+    redirect "/lists/#{id}"
+  end
+end
+
+# Delete an existing list
+post '/lists/:id/destroy' do
+  id = params[:id].to_i
+  @list=load_list(id)
+  session[:lists].delete(@list)
+  redirect "/lists/#{id}"
+end
+
+# Create a new todo item
+post "/lists/:list_id/todos" do
+  text = params[:todo]
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  id = next_element_id(@list[:todos])
+  @list[:todos] << {id: id, name: text, completed: false}
+  session[:success] = "The todo was added."
+  redirect "/lists/#{@list_id}"
+end
+
+# Delete a todo item
+post "/lists/:list_id/todos/:id/destroy" do
+  @list_id = params[:list_id].to_i
+  @list = load_list(@list_id)
+  todo_id = params[:id].to_i
+
+  @list[:todos].delete_at todo_id
+  session[:success] = "The todo has been deleted."
+  redirect "/lists/#{@list_id}"
+end
+
+# Update status of todo id
+post "/lists/:list_id/todos/:id" do
+  @list_id = params[:list_id].to_i
+  @list = load_list(@list_id)
+  todo_id = params[:id].to_i 
+
+  is_completed = params[:completed] == "true"
+  @list[:todos][todo_id][:completed] = is_completed
+  session[:success] = "The todo has been updated."
+  redirect "/lists/#{@list_id}"
+end
+
+# Mark all todos on the list as completed
+post '/lists/:id/complete_all' do
+  @list_id = params[:id].to_i
+  @list = load_list(@list_id)
+  @list[:todos].each do |todo|
+    todo[:completed] = true
+  end
+
+  session[:success] = "All todos have been completed."
+  redirect "/lists/#{@list_id}"
+end
+
+
